c8b9634c8c6e1546603e5adb5c83159148f212af
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-03-12 23:37:16 +0600

    handle error with our personal error handler

--- src/app/module/auth/auth.services.ts
+++ src/app/module/auth/auth.services.ts
@@ -1,70 +1,74 @@
+import status from 'http-status'
 import {
-	Role,
-	User,
-	UserStatus
+  Role,
+  User,
+  UserStatus
 } from '../../../../generated/prisma/client/client'
+import AppError from '../../errorHelpers/AppError'
 import { auth } from '../../lib/auth'
 import { prisma } from '../../lib/prisma'
 
 const registerPatient = async (payload: User & { password: string }) => {
-	const { name, email, password } = payload
-	const data = await auth.api.signUpEmail({
-		body: {
-			name,
-			email,
-			password
-			//* this value have default value so you can skip it also
-			// needPasswordChange: false,
-			// role: Role.PATIENT
-		}
-	})
-	if (!data.user) {
-		throw new Error('Failed to Register')
-	}
-	try {
-		const patient = await prisma.$transaction(async (tx) => {
-			const patientTx = await tx.patient.create({
-				data: {
-					userId: data.user.id,
-					name,
-					email
-				}
-			})
-			return patientTx
-		})
-		return { ...data, patient }
-	} catch (error) {
-		console.log('Transaction error : ', error)
-		await prisma.user.delete({
-			where: {
-				id: data.user.id
-			}
-		})
-		throw error
-	}
+  const { name, email, password } = payload
+  const data = await auth.api.signUpEmail({
+    body: {
+      name,
+      email,
+      password
+      //* this value have default value so you can skip it also
+      // needPasswordChange: false,
+      // role: Role.PATIENT
+    }
+  })
+  if (!data.user) {
+    // throw new Error('Failed to Register')
+    throw new AppError(status.BAD_REQUEST, 'Failed to Register')
+  }
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      const patientTx = await tx.patient.create({
+        data: {
+          userId: data.user.id,
+          name,
+          email
+        }
+      })
+      return patientTx
+    })
+    return { ...data, patient }
+  } catch (error) {
+    console.log('Transaction error : ', error)
+    await prisma.user.delete({
+      where: {
+        id: data.user.id
+      }
+    })
+    throw error
+  }
 }
 
 const loginUser = async (payload: User & { password: string }) => {
-	const { email, password } = payload
-	const data = await auth.api.signInEmail({
-		body: {
-			email,
-			password
-		}
-	})
-	if (!data.user) {
-		throw new Error('Failed to Login')
-	}
-	if (data.user.status !== UserStatus.ACTIVE) {
-		throw new Error('User is not active')
-	}
-	if (data.user.isDeleted) {
-		throw new Error('User is deleted')
-	}
-	return data
+  const { email, password } = payload
+  const data = await auth.api.signInEmail({
+    body: {
+      email,
+      password
+    }
+  })
+  if (!data.user) {
+    // throw new Error('Failed to Login')
+    throw new AppError(status.BAD_REQUEST, 'Failed to Login')
+  }
+  if (data.user.status !== UserStatus.ACTIVE) {
+    throw new AppError(status.FORBIDDEN, 'User is not active')
+  }
+  if (data.user.isDeleted) {
+    throw new AppError(status.GONE, 'User is deleted')
+  }
+  return data
 }
 
 export const AuthService = {
-	registerPatient,
-	loginUser
+  registerPatient,
+  loginUser
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,55 +1,61 @@
 import dotenv from 'dotenv'
+import AppError from '../app/errorHelpers/AppError'
+import status from 'http-status'
 
 dotenv.config()
 
 interface EnvConfig {
-	PORT: number
-	NODE_ENV: string
-	DATABASE_URL: string
-	BETTER_AUTH_SECRET: string
-	BETTER_AUTH_URL: string
-	ACCESS_TOKEN_SECRET: string
-	REFRESH_TOKEN_SECRET: string
-	ACCESS_TOKEN_EXPIRES_IN: string
-	REFRESH_TOKEN_EXPIRES_IN: string
-	BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string
-	BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string
+  PORT: number
+  NODE_ENV: string
+  DATABASE_URL: string
+  BETTER_AUTH_SECRET: string
+  BETTER_AUTH_URL: string
+  ACCESS_TOKEN_SECRET: string
+  REFRESH_TOKEN_SECRET: string
+  ACCESS_TOKEN_EXPIRES_IN: string
+  REFRESH_TOKEN_EXPIRES_IN: string
+  BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string
+  BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string
 }
 
 const envConfig = (): EnvConfig => {
-	const requiredEnvVariables = [
-		'PORT',
-		'NODE_ENV',
-		'DATABASE_URL',
-		'BETTER_AUTH_SECRET',
-		'BETTER_AUTH_URL',
-		'ACCESS_TOKEN_SECRET',
-		'REFRESH_TOKEN_SECRET',
-		'ACCESS_TOKEN_EXPIRES_IN',
-		'REFRESH_TOKEN_EXPIRES_IN',
-		'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
-		'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE'
-	]
-	requiredEnvVariables.forEach((envVar) => {
-		if (!process.env[envVar]) {
-			throw new Error(`Environment variable ${envVar} is required`)
-		}
-	})
-	return {
-		PORT: parseInt(process.env.PORT || '5000'),
-		NODE_ENV: process.env.NODE_ENV || 'development',
-		DATABASE_URL: process.env.DATABASE_URL || '',
-		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET || '',
-		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL || '',
-		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
-		REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
-		ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
-		REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
-		BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env
-			.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
-		BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env
-			.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string
-	}
+  const requiredEnvVariables = [
+    'PORT',
+    'NODE_ENV',
+    'DATABASE_URL',
+    'BETTER_AUTH_SECRET',
+    'BETTER_AUTH_URL',
+    'ACCESS_TOKEN_SECRET',
+    'REFRESH_TOKEN_SECRET',
+    'ACCESS_TOKEN_EXPIRES_IN',
+    'REFRESH_TOKEN_EXPIRES_IN',
+    'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
+    'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE'
+  ]
+  requiredEnvVariables.forEach((envVar) => {
+    if (!process.env[envVar]) {
+      // throw new Error(`Environment variable ${envVar} is required`)
+      throw new AppError(
+        status.INTERNAL_SERVER_ERROR,
+        `Environment variable ${envVar} is required`
+      )
+    }
+  })
+  return {
+    PORT: parseInt(process.env.PORT || '5000'),
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || '',
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET || '',
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL || '',
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+    BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env
+      .BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
+    BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env
+      .BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string
+  }
 }
 
 export const envVariables = envConfig()
