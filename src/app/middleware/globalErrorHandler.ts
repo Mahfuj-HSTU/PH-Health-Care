b9a30e3b918233981fc5c4eb315e229b00972f18
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-03-12 23:38:01 +0600

    update global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,79 +1,82 @@
-import type { NextFunction, Request, Response } from 'express'
-import { error } from 'node:console'
-import { Prisma } from '../../../generated/prisma/client'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express'
+import status from 'http-status'
 import z from 'zod'
-import { TErrorSources } from '../interfaces/error.interfaces'
+import AppError from '../errorHelpers/AppError'
 import { handleZodError } from '../errorHelpers/handleZodError'
+import { envVariables } from '../../config/env'
+import { TErrorResponse, TErrorSources } from '../interfaces/error.interfaces'
 
-function globalErrorHandler(
-	err: any,
-	req: Request,
-	res: Response,
-	next: NextFunction
-) {
-	let statusCode = 500
-	let errormessage = 'Internal Server Error'
-	let errorDetails = err
-	const errorSource: TErrorSources[] = []
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (envVariables.NODE_ENV === 'development') {
+    console.log('Error from Global Error Handler', err)
+  }
 
-	if (err instanceof z.ZodError) {
-		const simplifiedError = handleZodError(err)
-		statusCode = simplifiedError.statusCode
-		errormessage = simplifiedError.message
-		// err.issues.forEach((issue: any) => {
-		// 	errorSource.push({
-		// 		path: issue.path.join('.'),
-		// 		message: issue.message
-		// 	})
-		// })
-		errorSource.push(...simplifiedError.errorSources)
-	}
+  let errorSources: TErrorSources[] = []
+  let statusCode: number = status.INTERNAL_SERVER_ERROR
+  let message: string = 'Internal Server Error'
+  let stack: string | undefined = undefined
 
-	// *prisma validation error
-	if (err instanceof Prisma.PrismaClientValidationError) {
-		statusCode = 400
-		errormessage = 'You have made an invalid request to the database.'
-	} else if (err instanceof Prisma.PrismaClientKnownRequestError) {
-		// *prisma known error
-		if (err.code === 'P2002') {
-			statusCode = 409
-			errormessage = 'Unique constraint failed.'
-		} else if (err.code === 'P2025') {
-			statusCode = 404
-			errormessage = 'The requested record was not found.'
-		} else if (err.code === 'P2003') {
-			statusCode = 400
-			errormessage = 'Foreign key constraint failed.'
-		}
-	} else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
-		statusCode = 500
-		errormessage = 'An unknown error occurred with the database client.'
-	} else if (err instanceof Prisma.PrismaClientRustPanicError) {
-		statusCode = 500
-		errormessage = 'A panic occurred in the Prisma Client Rust engine.'
-	} else if (err instanceof Prisma.PrismaClientInitializationError) {
-		if (err.errorCode === 'P1000') {
-			statusCode = 401
-			errormessage = 'Authentication failed against the database.'
-		} else if (err.errorCode === 'P1001') {
-			statusCode = 503
-			errormessage = 'The database server is not available.'
-		} else if (err.errorCode === 'P1002') {
-			statusCode = 503
-			errormessage = 'The database timed out while trying to connect.'
-		} else {
-			statusCode = 500
-			errormessage =
-				'An error occurred during the initialization of the Prisma Client.'
-		}
-	}
-	res.status(statusCode)
-	res.json({
-		success: false,
-		message: errormessage,
-		stack: errorSource,
-		error: errorDetails.message
-	})
+  //Zod Error Patttern
+  /*
+     error.issues; 
+    /* [
+      {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' , 'password' ], => username password
+        message: 'Invalid input: expected string'
+      },
+      {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+      }
+    ] 
+    */
+
+  if (err instanceof z.ZodError) {
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack
+  } else if (err instanceof AppError) {
+    statusCode = err.statusCode
+    message = err.message
+    stack = err.stack
+    errorSources = [
+      {
+        path: '',
+        message: err.message
+      }
+    ]
+  } else if (err instanceof Error) {
+    statusCode = status.INTERNAL_SERVER_ERROR
+    message = err.message
+    stack = err.stack
+    errorSources = [
+      {
+        path: '',
+        message: err.message
+      }
+    ]
+  }
+
+  const errorResponse: TErrorResponse = {
+    success: false,
+    message: message,
+    errorSources,
+    error: envVariables.NODE_ENV === 'development' ? err : undefined,
+    stack: envVariables.NODE_ENV === 'development' ? stack : undefined
+  }
+
+  res.status(statusCode).json(errorResponse)
 }
-
-export default globalErrorHandler
