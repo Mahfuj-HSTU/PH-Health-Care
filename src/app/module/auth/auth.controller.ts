053daf9a55cd177f33c5229a826f60c9df819721
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-03-13 12:09:30 +0600

    update auth controller with tokens

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -1,28 +1,63 @@
+import { Request, Response } from 'express'
+import status from 'http-status'
+import ms, { StringValue } from 'ms'
 import catchAsync from '../../helper/catchAsync'
-import sendResponse from '../../helper/sendResponse'
+import { envVariables } from '../../../config/env'
 import { AuthService } from './auth.services'
+import { tokenUtils } from '../../utils/token'
+import sendResponse from '../../helper/sendResponse'
 
-const registerPatient = catchAsync(async (req, res, next) => {
-	const user = await AuthService.registerPatient(req.body)
-	sendResponse(res, {
-		httpStatuscode: 200,
-		success: true,
-		message: 'User created successfully',
-		data: user
-	})
+const registerPatient = catchAsync(async (req: Request, res: Response) => {
+  const maxAge = ms(envVariables.ACCESS_TOKEN_EXPIRES_IN as StringValue)
+  console.log({ maxAge })
+  const payload = req.body
+
+  console.log(payload)
+
+  const result = await AuthService.registerPatient(payload)
+
+  const { accessToken, refreshToken, token, ...rest } = result
+
+  tokenUtils.setAccessTokenCookie(res, accessToken)
+  tokenUtils.setRefreshTokenCookie(res, refreshToken)
+  tokenUtils.setBetterAuthSessionCookie(res, token as string)
+
+  sendResponse(res, {
+    httpStatusCode: status.CREATED,
+    success: true,
+    message: 'Patient registered successfully',
+    data: {
+      token,
+      accessToken,
+      refreshToken,
+      ...rest
+    }
+  })
 })
 
-const loginUser = catchAsync(async (req, res, next) => {
-	const user = await AuthService.loginUser(req.body)
-	sendResponse(res, {
-		httpStatuscode: 200,
-		success: true,
-		message: 'User logged in successfully',
-		data: user
-	})
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body
+  const result = await AuthService.loginUser(payload)
+  const { accessToken, refreshToken, token, ...rest } = result
+
+  tokenUtils.setAccessTokenCookie(res, accessToken)
+  tokenUtils.setRefreshTokenCookie(res, refreshToken)
+  tokenUtils.setBetterAuthSessionCookie(res, token)
+
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: 'User logged in successfully',
+    data: {
+      token,
+      accessToken,
+      refreshToken,
+      ...rest
+    }
+  })
 })
 
 export const AuthController = {
-	registerPatient,
-	loginUser
+  registerPatient,
+  loginUser
 }
